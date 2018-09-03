0da0581307a65d36b22b4e86ba96de514fbb0a68
Malak Abu Hammad <malak.abuhammad@gmail.com>
2018-09-03 19:19:45 +0300

    transactions

--- client/source/services/transactions.js
+++ client/source/services/transactions.js
@@ -1,35 +1,56 @@
 import {
-  Transaction,
-  TransactionHeader,
-  Batch,
-  BatchHeader,
-  BatchList
-} from 'sawtooth-sdk/protobuf';
-import { createHash } from 'crypto';
-import { getPublicKey, sign } from './signing.js';
-import { encode } from './encoding.js';
+    Transaction,
+    TransactionHeader,
+    Batch,
+    BatchHeader,
+    BatchList
+} from "sawtooth-sdk/protobuf";
+import {randomBytes, createHash} from "crypto";
+import {getPublicKey, sign} from "./signing.js";
+import {encode} from "./encoding.js";
 
 
-const FAMILY_NAME = 'cryptomoji';
-const FAMILY_VERSION = '0.1';
-const NAMESPACE = '5f4d76';
+const FAMILY_NAME = "cryptomoji";
+const FAMILY_VERSION = "0.1";
+const NAMESPACE = "5f4d76";
 
+export const hash512 = (str) => {
+    return createHash("sha512").update(str).digest("hex");
+}
 /**
  * A function that takes a private key and a payload and returns a new
  * signed Transaction instance.
  *
  * Hint:
  *   Remember ProtobufJS has two different APIs for encoding protobufs
- *   (which you'll use for the TransactionHeader) and for creating
- *   protobuf instances (which you'll use for the Transaction itself):
+ *   (which you"ll use for the TransactionHeader) and for creating
+ *   protobuf instances (which you"ll use for the Transaction itself):
  *     - TransactionHeader.encode({ ... }).finish()
  *     - Transaction.create({ ... })
  *
- *   Also, don't forget to encode your payload!
+ *   Also, don"t forget to encode your payload!
  */
 export const createTransaction = (privateKey, payload) => {
-  // Enter your solution here
+    const encodedPayload = encode(payload);
+    const publicKey = getPublicKey(privateKey);
+    const hashedPayload = createHash("sha512").update(encodedPayload).digest("hex");
 
+    const header = TransactionHeader.encode({
+        familyName: FAMILY_NAME,
+        familyVersion: FAMILY_VERSION,
+        signerPublicKey: publicKey,
+        batcherPublicKey: publicKey,
+        inputs: [NAMESPACE],
+        outputs: [NAMESPACE],
+        dependencies: [],
+        payloadSha512: hashedPayload,
+        nonce: randomBytes(32).toString() ,
+    }).finish();
+    return Transaction.create({
+        header,
+        headerSignature: sign(privateKey, header),
+        payload: encodedPayload
+    });
 };
 
 /**
@@ -40,7 +61,7 @@ export const createTransaction = (privateKey, payload) => {
  * transaction with no array.
  */
 export const createBatch = (privateKey, transactions) => {
-  // Your code here
+    // Your code here
 
 };
 
@@ -48,19 +69,19 @@ export const createBatch = (privateKey, transactions) => {
  * A fairly simple function that takes a one or more Batch instances and
  * returns an encoded BatchList.
  *
- * Although there isn't much to it, axios has a bug when POSTing the generated
- * Buffer. We've implemented it for you, transforming the Buffer so axios
+ * Although there isn"t much to it, axios has a bug when POSTing the generated
+ * Buffer. We"ve implemented it for you, transforming the Buffer so axios
  * can handle it.
  */
 export const encodeBatches = batches => {
-  if (!Array.isArray(batches)) {
-    batches = [ batches ];
-  }
-  const batchList = BatchList.encode({ batches }).finish();
+    if (!Array.isArray(batches)) {
+        batches = [batches];
+    }
+    const batchList = BatchList.encode({batches}).finish();
 
-  // Axios will mishandle a Uint8Array constructed with a large ArrayBuffer.
-  // The easiest workaround is to take a slice of the array.
-  return batchList.slice();
+    // Axios will mishandle a Uint8Array constructed with a large ArrayBuffer.
+    // The easiest workaround is to take a slice of the array.
+    return batchList.slice();
 };
 
 /**
@@ -73,6 +94,6 @@ export const encodeBatches = batches => {
  * multiple payloads in an array.
  */
 export const encodeAll = (privateKey, payloads) => {
-  // Your code here
+    // Your code here
 
 };

8d386ad9043eb7733e3a743b043b9cf5312563bc
Malak Abu Hammad <malak.abuhammad@gmail.com>
2018-09-04 17:17:08 +0300

    services + handler

--- processor/handler.js
+++ processor/handler.js
@@ -1,56 +1,77 @@
-'use strict';
+//"use strict";
 
-const { TransactionHandler } = require('sawtooth-sdk/processor/handler');
-const { InvalidTransaction } = require('sawtooth-sdk/processor/exceptions');
-const { decode } = require('./services/encoding');
+const {TransactionHandler} = require("sawtooth-sdk/processor/handler");
+const {InvalidTransaction} = require("sawtooth-sdk/processor/exceptions");
+const {decode} = require("./services/encoding");
 
 
-const FAMILY_NAME = 'cryptomoji';
-const FAMILY_VERSION = '0.1';
-const NAMESPACE = '5f4d76';
+const FAMILY_NAME = "cryptomoji";
+const FAMILY_VERSION = "0.1";
+const NAMESPACE = "5f4d76";
+const ALLOWED_ACTIONS = ['create', 'transfer', 'delete'];
 
 /**
  * A Cryptomoji specific version of a Hyperledger Sawtooth Transaction Handler.
  */
 class MojiHandler extends TransactionHandler {
-  /**
-   * The constructor for a TransactionHandler simply registers it with the
-   * validator, declaring which family name, versions, and namespaces it
-   * expects to handle. We'll fill this one in for you.
-   */
-  constructor () {
-    console.log('Initializing cryptomoji handler with namespace:', NAMESPACE);
-    super(FAMILY_NAME, [ FAMILY_VERSION ], [ NAMESPACE ]);
-  }
+    /**
+     * The constructor for a TransactionHandler simply registers it with the
+     * validator, declaring which family name, versions, and namespaces it
+     * expects to handle. We"ll fill this one in for you.
+     */
+    constructor() {
+        console.log("Initializing cryptomoji handler with namespace:", NAMESPACE);
+        super(FAMILY_NAME, [FAMILY_VERSION], [NAMESPACE]);
+    }
 
-  /**
-   * The apply method is where the vast majority of all the work of a
-   * transaction processor happens. It will be called once for every
-   * transaction, passing two objects: a transaction process request ("txn" for
-   * short) and state context.
-   *
-   * Properties of `txn`:
-   *   - txn.payload: the encoded payload sent from your client
-   *   - txn.header: the decoded TransactionHeader for this transaction
-   *   - txn.signature: the hex signature of the header
-   *
-   * Methods of `context`:
-   *   - context.getState(addresses): takes an array of addresses and returns
-   *     a Promise which will resolve with the requested state. The state
-   *     object will have keys which are addresses, and values that are encoded
-   *     state resources.
-   *   - context.setState(updates): takes an update object and returns a
-   *     Promise which will resolve with an array of the successfully
-   *     updated addresses. The updates object should have keys which are
-   *     addresses, and values which are encoded state resources.
-   *   - context.deleteState(addresses): deletes the state for the passed
-   *     array of state addresses. Only needed if attempting the extra credit.
-   */
-  apply (txn, context) {
-    // Enter your solution here
-    // (start by decoding your payload and checking which action it has)
 
-  }
+    /**
+     * The apply method is where the vast majority of all the work of a
+     * transaction processor happens. It will be called once for every
+     * transaction, passing two objects: a transaction process request ("txn" for
+     * short) and state context.
+     *
+     * Properties of `txn`:
+     *   - txn.payload: the encoded payload sent from your client
+     *   - txn.header: the decoded TransactionHeader for this transaction
+     *   - txn.signature: the hex signature of the header
+     *
+     * Methods of `context`:
+     *   - context.getState(addresses): takes an array of addresses and returns
+     *     a Promise which will resolve with the requested state. The state
+     *     object will have keys which are addresses, and values that are encoded
+     *     state resources.
+     *   - context.setState(updates): takes an update object and returns a
+     *     Promise which will resolve with an array of the successfully
+     *     updated addresses. The updates object should have keys which are
+     *     addresses, and values which are encoded state resources.
+     *   - context.deleteState(addresses): deletes the state for the passed
+     *     array of state addresses. Only needed if attempting the extra credit.
+     */
+    apply(transaction, context) {
+        let payload;
+        let header;
+        let signer;
+        return new Promise((resolve, reject) => {
+            header = transaction.header;
+            signer = header.signerPublicKey;
+            try {
+                payload = decode(transaction.payload);
+                resolve({
+                    action: payload[0],
+                    space: payload[1],
+                    signer: signer
+                });
+            } catch (err) {
+                let reason = new InvalidTransaction("Poorly encoded payload.");
+                reject(reason);
+            }
+        }).then(() => {
+            if (ALLOWED_ACTIONS.indexOf(payload.action) === -1) {
+                throw new InvalidTransaction("Not allowed action");
+            }
+        });
+    }
 }
 
 module.exports = MojiHandler;
--- processor/tests/02-MojiHandler.js
+++ processor/tests/02-MojiHandler.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 
-const { expect } = require('chai');
-const { InvalidTransaction } = require('sawtooth-sdk/processor/exceptions');
+const { expect } = require("chai");
+const { InvalidTransaction } = require("sawtooth-sdk/processor/exceptions");
 
-const MojiHandler = require('./services/handler_wrapper');
-const Txn = require('./services/mock_txn');
-const Context = require('./services/mock_context');
+const MojiHandler = require("./services/handler_wrapper");
+const Txn = require("./services/mock_txn");
+const Context = require("./services/mock_context");
 
 
-describe('Core MojiHandler Behavior', function() {
+describe("Core MojiHandler Behavior", function() {
   let handler = null;
   let context = null;
 
@@ -20,18 +20,18 @@ describe('Core MojiHandler Behavior', function() {
     context = new Context();
   });
 
-  it('should return a Promise', function() {
-    const txn = new Txn({ hello: 'world' });
+  it("should return a Promise", function() {
+    const txn = new Txn({ hello: "world" });
     const applyResult = handler.apply(txn, context);
 
     expect(applyResult).to.be.an.instanceOf(Promise);
     applyResult.catch(() => {});
   });
 
-  it('should reject poorly encoded payloads', function() {
+  it("should reject poorly encoded payloads", function() {
     const txn = new Txn({});
     // If not JSON stringified, an object will become `"[object Object]"`
-    txn.payload = Buffer.from({ hello: 'world' }.toString());
+    txn.payload = Buffer.from({ hello: "world" }.toString());
 
     return handler.apply(txn, context)
       .catch(err => {
@@ -39,12 +39,12 @@ describe('Core MojiHandler Behavior', function() {
         return true;
       })
       .then(wasRejected => {
-        expect(wasRejected, 'Transaction should be rejected').to.be.true;
+        expect(wasRejected, "Transaction should be rejected").to.be.true;
       });
   });
 
-  it('should reject unknown actions', function() {
-    const txn = new Txn({ action: 'BAD' });
+  it("should reject unknown actions", function() {
+    const txn = new Txn({ action: "BAD" });
 
     return handler.apply(txn, context)
       .catch(err => {
@@ -52,7 +52,7 @@ describe('Core MojiHandler Behavior', function() {
         return true;
       })
       .then(wasRejected => {
-        expect(wasRejected, 'Transaction should be rejected').to.be.true;
+        expect(wasRejected, "Transaction should be rejected").to.be.true;
       });
   });
 });
